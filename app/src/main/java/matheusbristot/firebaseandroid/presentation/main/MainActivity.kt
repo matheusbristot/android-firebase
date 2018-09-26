7cc7735d8b16c5b11b92def2ee84896d20a6b729
Matheus Bristot <matheus@jera.com.br>
2018-09-26 00:14:53 -0400

    Removed koin, changed readme, changed activity_main and MainActivity to databinding

--- app/src/main/java/matheusbristot/firebaseandroid/presentation/main/MainActivity.kt
+++ app/src/main/java/matheusbristot/firebaseandroid/presentation/main/MainActivity.kt
@@ -1,6 +1,8 @@
 package matheusbristot.firebaseandroid.presentation.main
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import matheusbristot.firebaseandroid.presentation.R
@@ -15,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.main = this // inicialização da main no xml
         mainViewModel = MainViewModel("Você esta na MainActivity")
         mainViewModel?.let {
             lifecycle.addObserver(it)
@@ -22,10 +25,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
     private fun onText(text: String?) {
         text?.let {
             binding.firstTextView.text = it
         }
+    }
+
+    //Devemos nos atentar, que este método não pode ser privado, se não o binding dará falha de compilação.
+    //E tbm, como estamos passando main::onClickTextView no xml, devemos deixar view: View como parametro,
+    //por conta do setOnClickListener, passar a view
+    fun onClickText(view: View) {
+        Toast.makeText(this, "[onClickText]", Toast.LENGTH_LONG).show()
     }
 }
